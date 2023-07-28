e12bb88d4135570fcacc626e1ad566630861ac41
emilym <emily.markova@gmail.com>
2023-07-28 10:27:52 -0700

    github test

--- about_me/ContentView.swift
+++ about_me/ContentView.swift
@@ -8,14 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var fun_fact = ""
+    @State private var button_content = "Click for fun fact :)"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Section(
+                header: Text("All About Me")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .background(.white)
+            ){
+                Image("cat")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.all)
+                Button(button_content) {
+                    if(button_content == "Click for fun fact :)"){
+                        fun_fact = "I have a black cat."
+                        button_content = "Click to erase fun fact :)"
+                    } else {
+                        fun_fact = ""
+                        button_content = "Click for fun fact :)"
+                    }
+                    
+                }
+                .padding(.all)
+                .border(.black)
+                .background(.white)
+
+                Text(fun_fact)
+            }
+            .background(.mint)
+            Section(
+                header: Text("Things I enjoy to do")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .background(.white)
+            ){
+                ScrollView{
+                    VStack{
+                        Image("snowboarding")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .padding(.all)
+                        Image("coding")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .padding(.all)
+                        Image("watching")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .padding(.all)
+                    }
+                }
+                
+
+            }
+            .background(.mint)
+            
+            
         }
-        .padding()
+        
     }
 }
 
